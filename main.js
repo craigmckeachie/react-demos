e7df88e0503bdc9c99323965b34472821e7488c9
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-21 09:20:23 -0400

    complete

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-function ID() {
-  return '_' + Math.random().toString(36).substr(2, 9);
-}
+const baseUrl = 'http://localhost:3000';
+const url = `${baseUrl}/photos`;
 
 function translateStatusToErrorMessage(status) {
   switch (status) {
     case 401:
       return 'Please login again.';
     case 403:
-      return 'You do not have permission to view the items.';
+      return 'You do not have permission to view the photos.';
     default:
-      return 'There was an error retrieving the items. Please try again.';
+      return 'There was an error retrieving the photos. Please try again.';
   }
 }
 
-//pass translate in to make this more flexible
 function checkStatus(response) {
   if (response.ok) {
     return response;
@@ -36,149 +34,40 @@ function parseJSON(response) {
   return response.json();
 }
 
-class Item {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-  }
+function delay(ms) {
+  return function (x) {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
 }
 
-const baseUrl = 'http://localhost:3000';
-const url = `${baseUrl}/items`;
-
-// API ----------
-const itemAPI = {
+const photoAPI = {
   getAll(page = 1, limit = 100) {
-    return fetch(`${url}?_page=${page}&_limit=${limit}`)
-      .then(checkStatus)
-      .then(parseJSON)
-      .catch((error) => {
-        let errorMessage = translateStatusToErrorMessage(error);
-        throw new Error(errorMessage);
-      });
-  },
-
-  add(item) {
-    return fetch(`${url}`, {
-      method: 'POST',
-      body: JSON.stringify(item),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(checkStatus)
-      .then(parseJSON);
-  },
-
-  update(item) {
-    return fetch(`${url}/${item.id}`, {
-      method: 'PUT',
-      body: JSON.stringify(item),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(checkStatus)
-      .then(parseJSON);
-  },
-
-  delete(id) {
-    return fetch(`${url}/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(checkStatus)
-      .then(parseJSON);
+    return (
+      fetch(`${url}?_page=${page}&_limit=${limit}`)
+        // .then(delay(600))
+        .then(checkStatus)
+        .then(parseJSON)
+        .catch((error) => {
+          let errorMessage = translateStatusToErrorMessage(error);
+          throw new Error(errorMessage);
+        })
+    );
   },
 };
 
-function List(props) {
-  const { items, onRemove, onUpdate, loading, error } = props;
-  const [editingItem, setEditingItem] = React.useState(null);
-
-  const handleEditClick = (item) => {
-    setEditingItem(item);
-  };
-
-  const handleCancel = () => {
-    setEditingItem(null);
-  };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  } else if (error) {
-    return <div>{error}</div>;
-  } else {
-    return (
-      <ul>
-        {items?.map((item) => (
-          <li key={item.id}>
-            {item === editingItem ? (
-              <Form item={item} onSubmit={onUpdate} onCancel={handleCancel} />
-            ) : (
-              <p>
-                <span>{item.name}</span>
-                <button onClick={() => handleEditClick(item)}>Edit</button>
-                <button onClick={() => onRemove(item)}>Remove</button>
-              </p>
-            )}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
-
-function Form({ item, onSubmit, onCancel, buttonValue }) {
-  const [inputValue, setInputValue] = React.useState(item.name || '');
-
-  const handleChange = (event) => {
-    event.preventDefault();
-    setInputValue(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const submittedItem = {
-      id: item ? item.id : ID(),
-      name: inputValue,
-    };
-
-    onSubmit(submittedItem);
-    setInputValue('');
-  };
-
-  const handleCancel = (event) => {
-    event.preventDefault();
-    onCancel();
-  };
-
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <input value={inputValue} onChange={handleChange} />
-      <button>{buttonValue || 'Save'}</button>
-      {onCancel && (
-        <a href="#" onClick={handleCancel}>
-          cancel
-        </a>
-      )}
-    </form>
-  );
-}
-
-function Container() {
-  const [items, setItems] = React.useState([]);
+function usePhotos() {
   const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState(undefined);
+  const [photos, setPhotos] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
     setLoading(true);
-    itemAPI
+    setError(null);
+
+    photoAPI
       .getAll(1)
       .then((data) => {
-        setItems(data);
+        setPhotos(data);
         setLoading(false);
       })
       .catch((error) => {
@@ -187,66 +76,30 @@ function Container() {
       });
   }, []);
 
-  const addItem = (item) => {
-    itemAPI
-      .add(item)
-      .then((newItem) => {
-        setItems([...items, newItem]);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
-
-  const updateItem = (updatedItem) => {
-    itemAPI
-      .update(updatedItem)
-      .then((data) => {
-        let updatedItems = items.map((item) => {
-          return item.id === updatedItem.id
-            ? Object.assign({}, item, data)
-            : item;
-        });
-        setItems(updatedItems);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
-
-  const removeItem = (removeThisItem) => {
-    itemAPI
-      .delete(removeThisItem.id)
-      .then(() => {
-        const filteredItems = items.filter(
-          (item) => item.id != removeThisItem.id
-        );
-        setItems(filteredItems);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
-
-  return (
-    <div>
-      <Form item="" onSubmit={addItem} buttonValue="Add" />
-      <List
-        loading={loading}
-        error={error}
-        items={items}
-        onRemove={removeItem}
-        onUpdate={updateItem}
-      />
-    </div>
-  );
+  return { loading, photos, error };
 }
 
-function App() {
-  return (
-    <div>
-      <Container />
-    </div>
-  );
+function PhotoList() {
+  const { loading, photos, error } = usePhotos();
+
+  if (error) {
+    return <div>{error}</div>;
+  } else if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {photos.map((photo) => {
+          return (
+            <li key={photo.id}>
+              <img src={photo.thumbnailUrl} alt={photo.title} />
+              <h3>{photo.title}</h3>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<PhotoList />, document.getElementById('root'));
